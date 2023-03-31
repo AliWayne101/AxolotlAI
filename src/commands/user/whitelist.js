004b73dc870745487ee0c2210b51690dabafd1c2
AliWayne101 <alimalikwayne@gmail.com>
2023-03-31 21:01:24 +0500

    fixed and added feature to add @everyone

--- src/commands/user/whitelist.js
+++ src/commands/user/whitelist.js
@@ -8,50 +8,75 @@ module.exports = {
       option.setName('user')
         .setDescription('@USER to add the user in whitelist')),
   async execute(interaction, client) {
-    var _reply = "";
     try {
-        var taggedUser = interaction.options.get('user').value;
-        if (taggedUser.includes("@")) {
-            taggedUser = taggedUser.split("@")[1].slice(0, -1);
-            const serverProfile = await Guild.findOne({ guildId: interaction.guild.id });
-            if (serverProfile) {
-                if (interaction.user.username === serverProfile.guildOwnerName && interaction.user.discriminator === serverProfile.guildOwnerDiscriminator) {
-                    var targetUser = interaction.guild.members.cache.find(({ id }) => id === taggedUser);
-                    if (targetUser.user.id !== interaction.user.id) {
-                        if (targetUser !== undefined) {
-                            var previousUsers = serverProfile.guildWhitelist;
-                            var checkExists = false;
-                            for (let userID of previousUsers) {
-                                if (userID === targetUser.user.id) checkExists = true;
-                            }
+        var taggedUserRaw = interaction.options.get('user').value;
+        if (!taggedUserRaw.includes("@")) {
+            await interaction.reply({
+                content: "You must tag the user, using @ symbol"
+            });
+            return;
+        }
 
-                            if (checkExists === false) {
-                                previousUsers.push(targetUser.user.id);
-                                console.log(`New userID: ${ targetUser.user.id }`);
-                                console.info(previousUsers);
-                                const res = await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { guildWhitelist: previousUsers }, { returnOriginal: false });
-                                console.log(res);
-                                _reply = `${ targetUser.user.username }#${ targetUser.user.discriminator } has been added to whitelist`;
-                            } else
-                                _reply = "User is already whitelisted";
-                        } else 
-                            _reply = "There are some technical issues, please try again";
-                    } else
-                        _reply = "You are already whitelisted";
-                } else
-                    _reply = "Only Owner of the server can whitelist users";
-            } else
-                _reply = "There are some technical issues, please try again";
+        const serverProfile = await Guild.findOne({ guildId: interaction.guild.id });
+        if (!serverProfile) {
+            await interaction.reply({
+                content: "Something went wrong, try again"
+            });
+            return;
+        }
+
+        const taggedUser = taggedUserRaw.split("@")[1].slice(0, -1);
+        if (interaction.user.username !== serverProfile.guildOwnerName && interaction.user.discriminator !== serverProfile.guildOwnerDiscriminator) {
+            await interaction.reply({
+                content: "Only the owner of the server can add people to whitelist"
+            });
+            return;
+        }
+
+        if (taggedUser === interaction.user.id) {
+            await interaction.reply({
+                content: "You are already whitelist, by default"
+            });
+            return;
+        }
+
+        if (taggedUserRaw === "@everyone") {
+            const guildMembersRaw = interaction.guild.members.cache;
+            const _whitelist = serverProfile.guildWhitelist;
+            const newList = [];
+            guildMembersRaw.forEach((member) => {
+                const exists = _whitelist.find( id => id === member.user.id);
+                if (exists === undefined)
+                    newList.push(member.user.id);
+            });
+            const eres = await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { guildWhitelist: newList }, { returnOriginal: false });
+            await interaction.reply({
+                content: `Everyone has been added to whitelist`
+            });
         } else {
-            _reply = "You must tag the user using @ symbol";
+            var prevUsers = serverProfile.guildWhitelist;
+            var userExists = prevUsers.find(id => id === taggedUser);
+            if (userExists !== undefined) {
+                await interaction.reply({
+                    content: "User is already whitelisted"
+                });
+                return;
+            }
+            const targetUser = interaction.guild.members.cache.find(({ id }) => id === taggedUser);
+            prevUsers.push(taggedUser);
+            console.log(`New userID: ${ taggedUser }`);
+            console.info(prevUsers);
+            const res = await Guild.findOneAndUpdate({ guildId: interaction.guild.id } , { guildWhitelist: prevUsers }, { returnOriginal: false });
+            console.log(res);
+            await interaction.reply({
+                content: `${ targetUser.user.username }#${ targetUser.user.discriminator } has been added to whitelist`
+            });
         }
     } catch (err) {
         console.log(err);
-        _reply = "Something went wrong, please try again";
+        await interaction.reply({
+          content: "Something went wrong, try again"
+        });
     }
-
-    await interaction.reply({
-      content: _reply
-    });
   }
 };
