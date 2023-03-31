f6b33af1c9302eb068d72b8e2859918683fe2e8b
AliWayne101 <alimalikwayne@gmail.com>
2023-03-31 20:08:48 +0500

    Fixed code and added feature to remove @everyone

--- src/commands/user/remove.js
+++ src/commands/user/remove.js
@@ -1,42 +1,90 @@
 const { SlashCommandBuilder } = require("discord.js");
-const Guild = require('../../schemas/guild');
+const Guild = require("../../schemas/guild");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("remove")
     .setDescription("Remove a user from whitelist from using this application")
-    .addStringOption(option =>
-      option.setName('user')
-        .setDescription('@USER to remove the user from whitelist')),
+    .addStringOption((option) =>
+      option
+        .setName("user")
+        .setDescription("@USER to remove the user from whitelist")
+    ),
   async execute(interaction, client) {
     var _reply = "";
     try {
-        var taggedUser = interaction.options.get('user').value;
-        if (taggedUser.includes("@")) {
-            taggedUser = taggedUser.split("@")[1].slice(0, -1);
-            const profileData = await Guild.findOne({ guildId: interaction.guild.id });
-            if (profileData) {
-                if (interaction.user.username === profileData.guildOwnerName && interaction.user.discriminator === profileData.guildOwnerDiscriminator) {
-                    const ifExists = profileData.guildWhitelist.find(targetID => targetID === taggedUser);
-                    if (ifExists !== undefined) {
-                        let newList = profileData.guildWhitelist.filter(targetID => targetID !== taggedUser);
-                        const updatedList = await Guild.findOneAndUpdate({ guildId: interaction.guild.id }, { guildWhitelist: newList });
-                        const userDetails = interaction.guild.members.cache.find(({ id }) => id === taggedUser).user;
-                        _reply = `${ userDetails.username }#${ userDetails.discriminator } has been removed from whitelist`;
-                    } else
-                        _reply = "User is not whitelisted";
-                } else
-                    _reply = "Only the Owner of the server can remove the user from whitelist";
-            } else
-                _reply = "Something went wrong, try again";
-        } else
-            _reply = "You must tag the user using @ symbol";
+      var taggedUserRaw = interaction.options.get("user").value;
+      if (!taggedUserRaw.includes("@")) {
+        await interaction.reply({
+          content: "You must tag the user, using @ symbol",
+        });
+        return;
+      }
+
+      var taggedUser = taggedUserRaw.split("@")[1].slice(0, -1);
+      const profileData = await Guild.findOne({
+        guildId: interaction.guild.id,
+      });
+      if (!profileData) {
+        await interaction.reply({
+          content: "Something went wrong, please try again",
+        });
+        return;
+      }
+
+      if (
+        interaction.user.username !== profileData.guildOwnerName &&
+        interaction.user.discriminator !== profileData.guildOwnerDiscriminator
+      ) {
+        await interaction.reply({
+          content:
+            "Only the owner of the server can remove people from whitelist",
+        });
+        return;
+      }
+
+      const ifExists = profileData.guildWhitelist.filter(
+        (targetID) => targetID === taggedUser
+      );
+      if (ifExists === undefined) {
+        await interaction.reply({
+          content: "User is not whitelisted",
+        });
+        return;
+      }
+
+      if (taggedUserRaw === "@everyone") {
+        const newArray = [];
+        newArray.push(interaction.user.id);
+        const updateEveryone = await Guild.findOneAndUpdate(
+          { guildId: interaction.guild.id },
+          { guildWhitelist: newArray }
+        );
+        await interaction.reply({
+          content: `Everyone has been removed from whitelist`,
+        });
+        return;
+      } else {
+        let newList = profileData.guildWhitelist.filter(
+          (targetID) => targetID !== taggedUser
+        );
+        const updatedList = await Guild.findOneAndUpdate(
+          { guildId: interaction.guild.id },
+          { guildWhitelist: newList }
+        );
+        const userDetails = interaction.guild.members.cache.find(
+          ({ id }) => id === taggedUser
+        ).user;
+        await interaction.reply({
+          content: `${userDetails.username}#${userDetails.discriminator} has been removed from whitelist`,
+        });
+      }
     } catch (err) {
-        console.log(err);
-        _reply = "Something went wrong, try again";
+      console.log(err);
+      _reply = "Something went wrong, try again";
     }
 
     await interaction.reply({
-        content: _reply
+      content: _reply,
     });
-  }
+  },
 };
